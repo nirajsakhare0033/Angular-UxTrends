b1be31c923b023b361773c8be65353caf9e4f40d
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-19 14:06:20 +0530

    property binding

--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-binding',
   templateUrl: './binding.component.html',
-  styleUrls: ['./binding.component.css']
+  styleUrls: ['./binding.component.css'],
 })
 export class BindingComponent {
-title:string=" Data binding";
-show(){
-  return "this is the method returning string and use in view"
-}
-condition:boolean=false;
+  //string interpolation
+
+  // title: string = ' Data binding';
+  // show() {
+  //   return 'this is the method returning string and use in view';
+  // }
+  // condition: boolean = false;
+
+  //string interpolation
+
+  property="property binding";
 }
