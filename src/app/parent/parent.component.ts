74693c01e106072c028954aed4df3b2880c3d62e
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-20 00:07:50 +0530

    ui created and data binding

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
+  productselected: boolean = false;
+  selectedprod: string;
 
+  alertShow: any;
+
+  onselectproduct(product) {
+    this.productselected = true;
+    this.selectedprod = product;
+  }
+  alertShowBtn(){
+    this.alertShow=this.selectedprod
+  }
 }
