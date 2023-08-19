deeab43e79c5d0a4ff682d7473a03bdd1065fc8b
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-19 13:37:45 +0530

    understanding component and creating header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MycontainerComponent } from './mycontainer/mycontainer.component';
+import { TopnavComponent } from './mycontainer/topnav/topnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MycontainerComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mycontainer/mycontainer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mycontainer',
+  templateUrl: './mycontainer.component.html',
+  styleUrls: ['./mycontainer.component.css']
+})
+export class MycontainerComponent {
+
+}
